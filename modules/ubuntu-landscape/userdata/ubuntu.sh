278be29ee59c76ec2247d34314843b1541f9a64b
Evandro Pomatti <evandro.pomatti@gmail.com>
2025-01-31 18:40:47 -0300

    feat: version variables

--- modules/ubuntu-landscape/userdata/ubuntu.sh
+++ modules/ubuntu-landscape/userdata/ubuntu.sh
@@ -5,11 +5,15 @@ export DEBIAN_FRONTEND=noninteractive
 apt update
 apt upgrade -y
 
+### Version
+ubuntuVersion=24.04
+awsCliArchitecture=aarch64 #ARM
+
 ### Required for Ubuntu Landscape
 apt install -y ca-certificates software-properties-common zip unzip
-add-apt-repository -y ppa:landscape/self-hosted-24.04 # https://ubuntu.com/landscape/docs/self-hosted-landscape
+add-apt-repository -y "ppa:landscape/self-hosted-$ubuntuVersion" # https://ubuntu.com/landscape/docs/self-hosted-landscape
 snap install certbot --classic
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-$awsCliArchitecture.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 ./aws/install
